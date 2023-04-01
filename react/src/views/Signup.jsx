7ffe34b34f713d3e675422c8ba6994de412de891
Rifkymoll <Rifkymaulana123@gmail.com>
2023-04-01 09:43:01 +0700

    create signup, login page, setup axios

--- react/src/views/Signup.jsx
+++ react/src/views/Signup.jsx
@@ -1,7 +1,56 @@
+import { Link } from "react-router-dom";
+import { useRef } from "react";
+import axiosClient from "../axios-client";
+import { useStateContext } from "../contexts/ContextProvider";
+
+
 export default function Signup() {
-    return (
-        <div>
-            Signup
-        </div>
-    )
+  const nameRef = useRef();
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const passwordConfirmationRef = useRef();
+
+  const {setUser, setToken} = useStateContext()
+
+  const onSubmit = (ev) => {
+    ev.preventDefault();
+    const payload = {
+      name: nameRef.current.value,
+      email: emailRef.current.value,
+      password: passwordRef.current.value,
+      password_confirmation: passwordConfirmationRef.current.value
+    }
+    axiosClient.post('/signup', payload)
+      .then(({data}) => {
+        setUser(data.token);
+        setToken(data.token);
+      })
+      .catch(err => {
+        const response = err.response;
+        if (response && response.status === 422) {
+          console.log(response.data.errors);
+        }
+      })
+    console.log(payload);
+  }
+
+  return (
+  <div className="login-signup-form animated fadeInDown">
+    <div className="form">
+      <form onSubmit={onSubmit}>
+        <h1 className="title">
+          Signup for free
+        </h1>
+        <input ref={nameRef} type="text" placeholder="Full Name" />
+        <input ref={emailRef} type="email" placeholder="Email Address" />
+        <input ref={passwordRef} type="password" placeholder="Password" />
+        <input ref={passwordConfirmationRef} type="password" placeholder="Password Confirmation" />
+        <button className="btn btn-block">Signup</button>
+        <p className="message">
+          Already Registered ? <Link to="/login">Sign in</Link>
+        </p>
+      </form>
+    </div>
+  </div>
+  )
 }
